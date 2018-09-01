6a5653024ec3cb919aa140780e05496153976d0f
NicolasRamirez9 <nicocai98@gmail.com>
2018-09-01 12:50:27 -0300

    TP casi terminado.

--- main.c
+++ main.c
@@ -8,11 +8,11 @@ float factorial (float , float);
 
 int main()
 {
-    int operacion;
-    float numeroUno;
-    float numeroDos;
+    int opciones;
+    float primerValor;
+    float segundoValor;
     float resultado;
-    int respuesta = 1;
+    char respuesta;
 
     do
     {
@@ -27,74 +27,115 @@ int main()
         printf ("5- Factoreo./n");
 
         printf ("Selecciona la operacion que desee ejecutar: ");
-        scanf ("%d" , &operacion);
+        scanf ("%d" , &opciones);
 
-        switch (operacion)
+        switch (opciones)
         {
         case 1:
             system ("cls");
             printf ("Ingrese el primer numero: ");
-            scanf ("%f" , &numeroUno);
+            scanf ("%f" , &primerValor);
             printf ("Ingrese el segundo numero: ");
-            scanf ("%f" , &numeroDos);
-            suma (numeroUno , numeroDos);
-            resultado = suma (numeroUno , numeroDos);
-            printf("El resultado de " , numeroUno , " + " , numeroDos , " es: %.2f \n" , resultado);
-            system ("cls");
+            scanf ("%f" , &segundoValor);
+            suma (primerValor , segundoValor);
+            resultado = suma (primerValor , segundoValor);
+            printf("El resultado de %2.f + %2.f es: %.2f \n" , primerValor , segundoValor , resultado);
             break;
 
         case 2:
             system ("cls");
             printf ("Ingrese el primer numero: ");
-            scanf ("%f" , &numeroUno);
+            scanf ("%f" , &primerValor);
             printf ("Ingrese el segundo numero: ");
-            scanf ("%f" , &numeroDos);
-            resta (numeroUno , numeroDos);
-            resultado = resta (numeroUno , numeroDos);
-            printf("El resultado de " , numeroUno , " - " , numeroDos , " es: %.2f \n" , resultado);
-            system ("cls");
+            scanf ("%f" , &segundoValor);
+            resta (primerValor , segundoValor);
+            resultado = resta (primerValor , segundoValor);
+            printf("El resultado de %2.f - %2.f es: %.2f \n" , primerValor , segundoValor , resultado);
             break;
 
         case 3:
             system ("cls");
             printf ("Ingrese el primer numero: ");
-            scanf ("%f" , &numeroUno);
+            scanf ("%f" , &primerValor);
             printf ("Ingrese el segundo numero: ");
-            scanf ("%f" , &numeroDos);
-            multiplicacion (numeroUno , numeroDos);
-            resultado = multiplicacion (numeroUno , numeroDos);
-            printf("El resultado de " , numeroUno , " * " , numeroDos , " es: %.2f \n" , resultado);
-            system("cls");
+            scanf ("%f" , &segundoValor);
+            multiplicacion (primerValor , segundoValor);
+            resultado = multiplicacion (primerValor , segundoValor);
+            printf("El resultado de %2.f * %2.f es: %.2f \n" , primerValor , segundoValor , resultado);
             break;
 
         case 4:
             system ("cls");
             printf ("Ingrese el primer numero: ");
-            scanf ("%f" , &numeroUno);
+            scanf ("%f" , &primerValor);
             printf ("Ingrese el segundo numero: ");
-            scanf ("%f" , &numeroDos);
-            division (numeroUno , numeroDos);
-            resultado = division (numeroUno , numeroDos);
-            printf("El resultado de " , numeroUno , " / " , numeroDos , " es: %.2f \n" , resultado);
-            system ("cls");
+            scanf ("%f" , &segundoValor);
+            division (primerValor , segundoValor);
             break;
 
         case 5:
             system ("cls");
             printf ("Ingrese el primer numero: ");
-            scanf ("%f" , &numeroUno);
+            scanf ("%f" , &primerValor);
             printf ("Ingrese el segundo numero: ");
-            scanf ("%f" , &numeroDos);
-            factorial (numeroUno , numeroDos);
-            resultado = factorial (numeroUno , numeroDos);
-            printf("El factorial de " , numeroUno , " es: %2.f ", resultado , " y el factorial de " , numeroDos , " es: %2.f " , resultado);
-            system ("cls");
+            scanf ("%f" , &segundoValor);
+            factorial (primerValor , segundoValor);
+            break;
+
+        case 6:
+            opciones = 6;
             break;
 
         default:
             printf("¡Ingrese una opcion valida! \n");
         }
-    }while(operacion != 6 );
+
+        printf ("¿Desea realizar otra operacion? (Ingrese 'N' o 'n' para salir.)");
+        fflush(stdin);
+        scanf("%c", &respuesta);
+        system("cls");
+
+    }while(respuesta != 'N' || respuesta != 'n');
 
     return 0;
+}
+float suma (float sumaUno , float sumaDos)
+{
+    float sumar;
+    sumar = sumaUno + sumaDos;
+    return sumar;
+}
+float resta (float restaUno , float restaDos)
+{
+    float restar;
+    restar = restaUno - restaDos;
+    return restar;
+}
+float multiplicacion (float multiplicarUno , float multiplicarDos)
+{
+    float multiplicar;
+    multiplicar = multiplicarUno * multiplicarDos;
+    return multiplicar;
+}
+float division(float dividirUno , float dividirDos)
+{
+    float dividir;
+    if (dividirDos != 0)
+    {
+        dividir = dividirUno / dividirDos;
+        printf("El resultado de %2.f / %2.f es: %.2f \n" , dividirUno , dividirDos , dividir);
+    }
+    else
+    {
+        printf("¡No es posible dividir por cero!");
+    }
+    return dividir;
+}
+float factorial(float numeroUno , float segundoValor)
+{
+    int i = 1;
+    float primerNumero = 0;
+    float segundoNumero = 0;
+
+    for (primerNumero = numeroUno ; primerNumero < 1 ; i--)
 }
